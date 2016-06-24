24b23b20b099752a6e2d27857dae1f599c8bed37
Ikki <takeda.i.aa@m.titech.ac.jp>
2016-06-24 20:57:39 +0900

    for文を中心にコードを短くした

--- VOL_intaractive.py
+++ VOL_intaractive.py
@@ -1,198 +1,184 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from tkinter import *
 import time
 import sympy as sp
 
-ep = 10**(-2)
-global size
-fullsize=500 #余白込みの画面のサイズ
-size = 300 #画面のサイズ
+ep = 10 ** (-2)
+fullsize = 500  # 余白込みの画面のサイズ
+size = 300  # 画面のサイズ
 
-edge= np.genfromtxt('edges-1.csv', delimiter=",").astype(np.int64)  #辺の情報
-global N
-N = np.max(edge).astype(np.int64)+1 #頂点の個数
-global eN
-eN = len(edge)  #辺の本数
+edge = np.genfromtxt('edges-1.csv', delimiter=",").astype(np.int64)  # 辺の情報
+N = np.max(edge).astype(np.int64) + 1  # 頂点の個数
+eN = len(edge)  # 辺の本数
 
-fix = np.zeros(N)
+fix = np.zeros(N)   # 固定するしないを記憶
 
-#nodeの初期値
-global node
-node = np.zeros((N,2))
-for i in range(N):
-    node[i] = [(fullsize+size*np.cos(2*np.pi*i/N))*0.5, (fullsize+size*np.sin(2*np.pi*i/N))*0.5]
-
-isConnect = np.zeros((N,N)) #繋がっている辺の組は1,繋がっていない組は0で表す
-for i in range(eN):
-    isConnect[edge[i][0]][edge[i][1]] = 1
-    isConnect[edge[i][1]][edge[i][0]] = 1
-
-#nodeの最短パスdを求める(Floyd法)
-d = isConnect
-for j in range(N):
-    for i in range(N):
-        if d[i][j] == 0:
-            if i != j:
-                d[i][j] = np.inf
-
-for m in range(N):
-    for i in range(N):
-        for j in range(N):
-            if d[i][m]+d[m][j]<d[i][j]:
-                d[i][j] = d[i][m]+d[m][j] 
-
-#ばねの自然長lを求める
-l = size/np.max(d)*d
-
-
-#kを求める
-k = np.zeros((N,N)) #ばね定数
-K = 1   #ばね定数の基準値
-for j in range(N):
-    for i in range(N):
-        if i != j:
-            k[i][j] = K/(d[i][j]**2)
-
-            
+#キャンパス
 root = Tk()
 
 w = Canvas(root, width=fullsize, height=fullsize, bg='White')
 w.pack()
-back = w.create_rectangle(0, 0, fullsize, fullsize, fill='White', outline='White', tags = 'back')
+back = w.create_rectangle(0, 0, fullsize, fullsize, fill='White', outline='White', tags='back')
 
 circles = []
 lines = []
 texts = []
-r=10
-N=len(node)
+r = 10
 
-#初期描画
+# nodeの初期値(for文を解消)
+global node
+node = (fullsize + size * np.c_[
+    np.cos(np.linspace(0, 2 * np.pi, N, endpoint=False)), np.sin(np.linspace(0, 2 * np.pi, N, endpoint=False))]) * 0.5
+
+isConnect = np.zeros((N, N))  # 繋がっている辺の組は1,繋がっていない組は0で表す
 for i in range(eN):
-    lines.append(w.create_line(node[edge[i][0]][0], node[edge[i][0]][1], 
-                           node[edge[i][1]][0], node[edge[i][1]][1], fill = 'Black', tags = 'edge'))
+    isConnect[edge[i][0]][edge[i][1]] = 1
+    isConnect[edge[i][1]][edge[i][0]] = 1
+
+# nodeの最短パスdを求める(Floyd法)
+d = isConnect
+for j in range(N):
+    for i in range(N):
+        if d[i][j] == 0:
+            d[i][j] = np.inf
+
+for m in range(N):
+    for i in range(N):
+        for j in range(N):
+            if d[i][m] + d[m][j] < d[i][j]:
+                d[i][j] = d[i][m] + d[m][j]
+
+                # ばねの自然長lを求める
+l = size / np.max(d) * d
+
+# kを求める
+K = 1  # ばね定数の基準値
+k = K / (d ** 2) * (np.ones((N, N)) - np.diag(np.ones(N)))
+
+# 初期描画
+for e in edge:
+    lines.append(w.create_line(node[e[0]][0], node[e[0]][1],
+                               node[e[1]][0], node[e[1]][1], fill='Black', tags='edge'))
 
 for i in range(N):
-    circles.append(w.create_oval(node[i][0]-r, node[i][1]-r, node[i][0]+r, node[i][1]+r, fill="White", tags = 'node'))
-    texts.append( w.create_text(node[i][0],node[i][1], text = str(i), fill='Black', tags = 'node'))
-    
-#sympy用の変数と方程式
+    circles.append(
+        w.create_oval(node[i][0] - r, node[i][1] - r, node[i][0] + r, node[i][1] + r, fill="White", tags='node'))
+    texts.append(w.create_text(node[i][0], node[i][1], text=str(i), fill='Black', tags='node'))
+
+# sympy用の変数と方程式
 sp.var('kij lij xi xj yi yj')
-sE = 1/2 * kij * ((xi-xj)**2 + (yi-yj)**2 + lij**2 - 2*lij*((xi-xj)**2 + (yi-yj)**2)**(1/2))
-sEx = sp.simplify(sp.diff(sE, xi))
-sEy = sp.simplify(sp.diff(sE, yi))
-sExx = sp.simplify(sp.diff(sEx, xi))
-sExy = sp.simplify(sp.diff(sEx, yi))
-sEyy = sp.simplify(sp.diff(sEy, yi))
-fEx = sp.lambdify((kij,lij,xi,xj,yi,yj), sEx)
-fEy = sp.lambdify((kij,lij,xi,xj,yi,yj), sEy)
-fExx = sp.lambdify((kij,lij,xi,xj,yi,yj), sExx)
-fExy = sp.lambdify((kij,lij,xi,xj,yi,yj), sExy)
-fEyy = sp.lambdify((kij,lij,xi,xj,yi,yj), sEyy)
+sE = 1 / 2 * kij * ((xi - xj) ** 2 + (yi - yj) ** 2 + lij ** 2 - 2 * lij * ((xi - xj) ** 2 + (yi - yj) ** 2) ** (1 / 2))
+sEx, sEy = [sp.diff(sE, z) for z in [xi, yi]]
+sExx, sExy = [sp.diff(sEx, z) for z in [xi, yi]]
+sEyy = sp.diff(sEy, yi)
+fEx, fEy, fExx, fExy, fEyy = [sp.lambdify((kij, lij, xi, xj, yi, yj), sp.simplify(s)) for s in
+                              [sEx, sEy, sExx, sExy, sEyy]]
+
 
 # 移動
+
 def move_node(event):
-    if val.get()==0:
+    if val.get() == 0:
         x = event.x
         y = event.y
         thisID = event.widget.find_withtag(CURRENT)[0]
 
-        #circleをクリックしていた場合
-        if (thisID-eN)%2 == 0: nodeID = int((thisID-eN-2)/2)
+        # circleをクリックしていた場合
+        if (thisID - eN) % 2 == 0:
+            nodeID = int((thisID - eN - 2) / 2)
 
-        #textをクリックしていた場合    
-        else: nodeID = int((thisID-eN-3)/2)
+        # textをクリックしていた場合
+        else:
+            nodeID = int((thisID - eN - 3) / 2)
 
-        #nodeの更新
-        node[nodeID] = [x,y]
+        # nodeの更新
+        node[nodeID] = [x, y]
+        update_node(x, y, nodeID)
 
-        #circle,text,lineの更新(描画の更新)
-        w.coords(circles[nodeID], x - r, y - r, x + r, y + r)
-        w.coords(texts[nodeID], x, y)   
-        for i in range(eN):
-            if edge[i][0] == nodeID:
-                w.coords(lines[i], x, y, node[edge[i][1]][0], node[edge[i][1]][1])
-            if edge[i][1] == nodeID:
-                w.coords(lines[i], node[edge[i][0]][0], node[edge[i][0]][1], x, y)
-            
+
+def update_node(x, y, nodeID):
+    # circle,text,lineの更新(描画の更新)
+    w.coords(circles[nodeID], x - r, y - r, x + r, y + r)
+    w.coords(texts[nodeID], x, y)
+    for i in range(eN):
+        if edge[i][0] == nodeID:
+            w.coords(lines[i], x, y, node[edge[i][1]][0], node[edge[i][1]][1])
+        if edge[i][1] == nodeID:
+            w.coords(lines[i], node[edge[i][0]][0], node[edge[i][0]][1], x, y)
+
+
 def click_edge(event):
-    if val.get()==0:
+    if val.get() == 0:
         global x0, y0, edgeID, n0, n1
         x0 = event.x
         y0 = event.y
         thisID = event.widget.find_withtag(CURRENT)[0]
         edgeID = thisID - 2
-        n0 = [node[edge[edgeID][0]][0],node[edge[edgeID][0]][1]]
-        n1 = [node[edge[edgeID][1]][0],node[edge[edgeID][1]][1]]
-            
+        n0 = [node[edge[edgeID][0]][0], node[edge[edgeID][0]][1]]
+        n1 = [node[edge[edgeID][1]][0], node[edge[edgeID][1]][1]]
+
+
 def move_edge(event):
-    if val.get()==0:
+    if val.get() == 0:
         x = event.x
         y = event.y
 
-        #nodeの更新
+        # nodeの更新
         node[edge[edgeID][0]] = [n0[0] + x - x0, n0[1] + y - y0]
         node[edge[edgeID][1]] = [n1[0] + x - x0, n1[1] + y - y0]
         n0new = node[edge[edgeID][0]]
         n1new = node[edge[edgeID][1]]
 
-        #circle,text,lineの更新(描画の更新)
-        w.coords(circles[edge[edgeID][0]], n0new[0] - r, n0new[1] - r, n0new[0] + r, n0new[1] + r)
-        w.coords(circles[edge[edgeID][1]], n1new[0] - r, n1new[1] - r, n1new[0] + r, n1new[1] + r)
-        w.coords(texts[edge[edgeID][0]], n0new[0], n0new[1])
-        w.coords(texts[edge[edgeID][1]], n1new[0], n1new[1])
-        w.coords(lines[edgeID], n0new[0], n0new[1], n1new[0], n1new[1])
-        for i in range(eN):
-            if edge[i][0] == edge[edgeID][0] and i != edgeID:
-                w.coords(lines[i], n0new[0], n0new[1], node[edge[i][1]][0], node[edge[i][1]][1])
-            elif edge[i][1] == edge[edgeID][0] and i != edgeID:
-                w.coords(lines[i], node[edge[i][0]][0], node[edge[i][0]][1], n0new[0], n0new[1])
-            elif edge[i][0] == edge[edgeID][1] and i != edgeID:
-                w.coords(lines[i], n1new[0], n1new[1], node[edge[i][1]][0], node[edge[i][1]][1])
-            elif edge[i][1] == edge[edgeID][1] and i != edgeID:
-                w.coords(lines[i], node[edge[i][0]][0], node[edge[i][0]][1], n1new[0], n1new[1])
+        # circle,text,lineの更新(描画の更新)
+        update_node(n0new[0], n0new[1], edge[edgeID][0])
+        update_node(n1new[0], n1new[1], edge[edgeID][1])
+
 
 def click_back(event):
-    if val.get()==0:
+    if val.get() == 0:
         global x0_, y0_, nall
         x0_ = event.x
         y0_ = event.y
-        nall=np.zeros((N,2))
+        nall = np.zeros((N, 2))
         for i in range(N):
-            nall[i] = [node[i][0],node[i][1]]
-            
+            nall[i] = [node[i][0], node[i][1]]
+
+
 def move_back(event):
-    if val.get()==0:
+    if val.get() == 0:
         x = event.x
         y = event.y
 
-        #nodeの更新
+        # nodeの更新
         for i in range(N):
             node[i] = [nall[i][0] + x - x0_, nall[i][1] + y - y0_]
 
-        #circle,text,lineの更新(描画の更新)
+        # circle,text,lineの更新(描画の更新)
         for i in range(eN):
             w.coords(lines[i], node[edge[i][0]][0], node[edge[i][0]][1], node[edge[i][1]][0], node[edge[i][1]][1])
         for i in range(N):
-            w.coords(circles[i], node[i][0]-r, node[i][1]-r, node[i][0]+r, node[i][1]+r)
-            w.coords(texts[i], node[i][0],node[i][1])
+            w.coords(circles[i], node[i][0] - r, node[i][1] - r, node[i][0] + r, node[i][1] + r)
+            w.coords(texts[i], node[i][0], node[i][1])
+
 
 def fix_node(event):
-    if val.get()==0:
+    if val.get() == 0:
         thisID = event.widget.find_withtag(CURRENT)[0]
 
-        #circleをクリックしていた場合
-        if (thisID-eN)%2 == 0: nodeID = int((thisID-eN-2)/2)
+        # circleをクリックしていた場合
+        if (thisID - eN) % 2 == 0:
+            nodeID = int((thisID - eN - 2) / 2)
 
-        #textをクリックしていた場合    
-        else: nodeID = int((thisID-eN-3)/2)
+        # textをクリックしていた場合
+        else:
+            nodeID = int((thisID - eN - 3) / 2)
 
-        if fix[nodeID] == 0:
-            fix[nodeID] = 1
+        fix[nodeID] = 1 - fix[nodeID]
+        if fix[nodeID] == 1:
             w.itemconfigure(circles[nodeID], fill='Yellow')
         else:
-            fix[nodeID] = 0
             w.itemconfigure(circles[nodeID], fill='White')
+
 
 def move_graph():
     # バインディング
@@ -203,80 +189,80 @@ def move_graph():
     w.tag_bind('back', '<Button1-Motion>', move_back)
     w.tag_bind('node', '<3>', fix_node)
 
+
 def anime_graph():
-    #dmを求める
+    # dmを求める
     global node
-    dm= np.zeros(N) #delta-m
-    Ex= np.zeros(N) #Ex
-    Ey= np.zeros(N) #Ey
+    dm = np.zeros(N)  # delta-m
+    Ex = np.zeros(N)  # Ex
+    Ey = np.zeros(N)  # Ey
 
     for m in range(N):
         for i in range(N):
-            if m != i and fix[m] ==0:
-                Ex[m]+= fEx(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
-                Ey[m]+= fEy(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
-    
+            if m != i and fix[m] == 0:
+                Ex[m] += fEx(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
+                Ey[m] += fEy(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
 
-    dm= (Ex**2+Ey**2)**(1/2)
+    dm = (Ex ** 2 + Ey ** 2) ** (1 / 2)
 
-    #dmが最大となるmを求める
+    # dmが最大となるmを求める
     for i in range(N):
         if np.max(dm) == dm[i]:
             m = i
-    
-    while np.max(dm)>ep and val.get()==1:
 
-        while dm[m]>ep and val.get()==1:
+    while np.max(dm) > ep and val.get() == 1:
 
-            #Ex,Ey,Exx,Exy,Eyyからdx,dyを求める
-            Ex[m]= 0
-            Ey[m]= 0
-            Exx= 0 #Exx
-            Exy= 0 #Exy, Eyx
-            Eyy= 0 #Eyy
+        while dm[m] > ep and val.get() == 1:
+
+            # Ex,Ey,Exx,Exy,Eyyからdx,dyを求める
+            Ex[m] = 0
+            Ey[m] = 0
+            Exx = 0  # Exx
+            Exy = 0  # Exy, Eyx
+            Eyy = 0  # Eyy
 
             for i in range(N):
-                if m != i and fix[m] ==0:
-                    Ex[m]+= fEx(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
-                    Ey[m]+= fEy(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
-                    Exx+= fExx(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
-                    Exy+= fExy(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
-                    Eyy+= fEyy(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
+                if m != i and fix[m] == 0:
+                    Ex[m] += fEx(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
+                    Ey[m] += fEy(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
+                    Exx += fExx(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
+                    Exy += fExy(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
+                    Eyy += fEyy(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
 
-            dx= (Ex[m]*Eyy-Ey[m]*Exy)/(Exy**2-Exx*Eyy) #dx
-            dy= (Ey[m]*Exx-Ex[m]*Exy)/(Exy**2-Exx*Eyy) #dy
+            dx = (Ex[m] * Eyy - Ey[m] * Exy) / (Exy ** 2 - Exx * Eyy)  # dx
+            dy = (Ey[m] * Exx - Ex[m] * Exy) / (Exy ** 2 - Exx * Eyy)  # dy
 
-            #node,dmを更新
-            node[m][0]+= dx
-            node[m][1]+= dy
-            dm[m]= (Ex[m]**2+Ey[m]**2)**(1/2)
-            
-            #描画の更新
+            # node,dmを更新
+            node[m][0] += dx
+            node[m][1] += dy
+            dm[m] = (Ex[m] ** 2 + Ey[m] ** 2) ** (1 / 2)
+
+            # 描画の更新
             time.sleep(0.1)
             w.coords(circles[m], node[m][0] - r, node[m][1] - r, node[m][0] + r, node[m][1] + r)
-            w.coords(texts[m], node[m][0], node[m][1])   
+            w.coords(texts[m], node[m][0], node[m][1])
             for i in range(eN):
                 if edge[i][0] == m:
                     w.coords(lines[i], node[m][0], node[m][1], node[edge[i][1]][0], node[edge[i][1]][1])
                 if edge[i][1] == m:
                     w.coords(lines[i], node[edge[i][0]][0], node[edge[i][0]][1], node[m][0], node[m][1])
-            w.pack() 
+            w.pack()
             w.update()
 
-        #dmを求める
-        dm= np.zeros(N) #delta-m
-        Ex= np.zeros(N) #Ex
-        Ey= np.zeros(N) #Ey
+        # dmを求める
+        dm = np.zeros(N)  # delta-m
+        Ex = np.zeros(N)  # Ex
+        Ey = np.zeros(N)  # Ey
 
         for m in range(N):
             for i in range(N):
-                if m != i and fix[m] ==0:
-                    Ex[m]+= fEx(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
-                    Ey[m]+= fEy(k[m][i],l[m][i],node[m][0],node[i][0],node[m][1],node[i][1])
+                if m != i and fix[m] == 0:
+                    Ex[m] += fEx(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
+                    Ey[m] += fEy(k[m][i], l[m][i], node[m][0], node[i][0], node[m][1], node[i][1])
 
-        dm= (Ex**2+Ey**2)**(1/2)
+        dm = (Ex ** 2 + Ey ** 2) ** (1 / 2)
 
-        #dmが最大となるmを求める
+        # dmが最大となるmを求める
         for i in range(N):
             if np.max(dm) == dm[i]:
                 m = i
@@ -286,9 +272,9 @@ val = IntVar()
 val.set(0)
 move_graph()
 
-r0 = Radiobutton(text = 'Drag / Stop', variable = val, value = 0, command = move_graph)
+r0 = Radiobutton(text='Drag / Stop', variable=val, value=0, command=move_graph)
 r0.pack()
-r1 = Radiobutton(text = 'Animation', variable = val, value = 1, command = anime_graph)
+r1 = Radiobutton(text='Animation', variable=val, value=1, command=anime_graph)
 r1.pack()
 
 root.mainloop()
