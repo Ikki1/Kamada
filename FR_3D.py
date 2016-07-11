829db3ed63b33ba503c92edabb1505ed866bc208
Ikki <takeda.i.aa@m.titech.ac.jp>
2016-07-11 18:04:05 +0900

    ｚ座標の値に応じて、nodeとedgeの色が変わるようにした

--- FR_3D.py
+++ FR_3D.py
@@ -27,11 +27,11 @@ shapes = {
 def initializeNode():
     global node_pos, node_disp
     #論文の手法
-    # node_pos = np.random.rand(N, dim) * size
-    # node_pos -= (sum(node_pos) / N - np.ones(dim) * fullsize / 2)
+    node_pos = np.random.rand(N, dim) * size
+    node_pos -= (sum(node_pos) / N - np.ones(dim) * fullsize / 2)
     #今回用いた手法
-    thetas = np.linspace(0, 2 * np.pi, N, endpoint=False)
-    node_pos = (fullsize + size * np.c_[np.cos(thetas), np.sin(thetas), np.random.rand(N)]) * 0.5
+    #thetas = np.linspace(0, 2 * np.pi, N, endpoint=False)
+    #node_pos = (fullsize + size * np.c_[np.cos(thetas), np.sin(thetas), np.random.rand(N)]) * 0.5
     node_disp =np.zeros((N, dim))
 
 initializeNode()
@@ -45,30 +45,49 @@ springConstant()
 
 def func_attractive(dist):
     length = 100
-    return dist ** 2 / k
+    return dist ** 2  / k
 
 def func_repulsive(dist):
     return k ** 2 / dist
 
 root = Tk()
 
+def colors(r, g, b):
+    c = '#' + ('%x' % int(r)).zfill(2) + ('%x' % int(g)).zfill(2) + ('%x' % int(b)).zfill(2)
+    return c
+
+color_num = 8
+
+def colormap(i):
+    maxcolor = np.array([255, 255, 255])
+    mincolor = np.array([50, 50, 100])
+    colorlist = np.array([np.linspace(maxcolor[i], mincolor[i], color_num, endpoint=True) for i in range(3)]).T
+    return colors(colorlist[i][0], colorlist[i][1], colorlist[i][2])
+
+def depth_to_color(z):
+    num = int(z / (fullsize / color_num))
+    if num <= 0: num = 0
+    elif num >= color_num: num = color_num - 1
+    return colormap(num)
+
 def initializeDraw():
     #キャンパス
     global window, radius
     window = Canvas(root, width=fullsize, height=fullsize, bg='White')
     window.pack()
-    back = window.create_rectangle(0, 0, fullsize, fullsize, fill='White', outline='White', tags='back')
+    back = window.create_rectangle(0, 0, fullsize, fullsize, fill='black', outline= '', tags='back')
     radius = 10
 
     # 初期描画
     for e in edge:
         shapes['lines'].append(window.create_line(node_pos[e[0]][0], node_pos[e[0]][1],
-                                   node_pos[e[1]][0], node_pos[e[1]][1], fill='Black', tags='edge'))
+                                   node_pos[e[1]][0], node_pos[e[1]][1],
+                                                  fill=depth_to_color((node_pos[e[0]][2] + node_pos[e[1]][2]) / 2), tags='edge'))
 
     for i in range(N):
         shapes['circles'].append(
             window.create_oval(node_pos[i][0] - radius, node_pos[i][1] - radius, node_pos[i][0] + radius, node_pos[i][1] + radius,
-                          fill="White", tags='node_pos'))
+                          fill=depth_to_color(node_pos[i][2]), outline= '', tags='node_pos'))
         shapes['texts'].append(window.create_text(node_pos[i][0], node_pos[i][1], text=str(i), fill='Black', tags='node'))
 
 initializeDraw()
@@ -96,12 +115,14 @@ def move_node(event):
 def update_node(x, y, nodeID):
     # circle,text,lineの更新(描画の更新)
     window.coords(shapes['circles'][nodeID], x - radius, y - radius, x + radius, y + radius)
+    window.itemconfigure(shapes['circles'][nodeID], fill = depth_to_color(node_pos[nodeID][2]))
     window.coords(shapes['texts'][nodeID], x, y)
     for i in range(eN):
         if edge[i][0] == nodeID:
             window.coords(shapes['lines'][i], x, y, node_pos[edge[i][1]][0], node_pos[edge[i][1]][1])
         if edge[i][1] == nodeID:
             window.coords(shapes['lines'][i], node_pos[edge[i][0]][0], node_pos[edge[i][0]][1], x, y)
+        window.itemconfigure(shapes['lines'][i], fill=depth_to_color((node_pos[edge[i][0]][2] + node_pos[edge[i][1]][2]) / 2))
 
 
 def click_edge(event):
@@ -149,14 +170,7 @@ def move_back(event):
         # nodeの更新
         for i in range(N):
             node_pos[i] = [nall[i][0] + x - x0_, nall[i][1] + y - y0_, node_pos[i][2]]
-            update_node(node_pos[i][0], node_pos[i][1],i)   #edgeの更新回数的にはupdate_nodeを使うのは非効率ではある
-
-        # # circle,text,lineの更新(描画の更新)
-        # for i in range(eN):
-        #     window.coords(lines[i], node_pos[edge[i][0]][0], node_pos[edge[i][0]][1], node_pos[edge[i][1]][0], node_pos[edge[i][1]][1])
-        # for i in range(N):
-        #     window.coords(circles[i], node_pos[i][0] - radius, node_pos[i][1] - radius, node_pos[i][0] + radius, node_pos[i][1] + radius)
-        #     window.coords(texts[i], node_pos[i][0], node_pos[i][1])
+            update_node(node_pos[i][0], node_pos[i][1],i)
 
 
 def fix_node(event):
@@ -173,9 +187,9 @@ def fix_node(event):
 
         fix[nodeID] = 1 - fix[nodeID]
         if fix[nodeID] == 1:
-            window.itemconfigure(shapes['circles'][nodeID], fill='Yellow')
+            window.itemconfigure(shapes['circles'][nodeID], outline='Yellow')
         else:
-            window.itemconfigure(shapes['circles'][nodeID], fill='White')
+            window.itemconfigure(shapes['circles'][nodeID], outline='')
 
 
 def move_graph():
@@ -196,7 +210,7 @@ def delta(i, j):
 
 def anime_graph():
     tempature = 1
-    #temp = 10
+    temp = 10
     active = True
     count = 0
     memo1 = np.ones((N, dim))
@@ -219,10 +233,10 @@ def anime_graph():
         for i in range(N):
             if fix[i] == 0:
                 #論文の手法
-                #node_pos[i] += node_disp[i] / np.linalg.norm(node_disp[i]) * min(temp ** 2, np.linalg.norm(node_disp[i]))
+                node_pos[i] += node_disp[i] / np.linalg.norm(node_disp[i]) * min(temp ** 2, np.linalg.norm(node_disp[i]))
                 #今回用いた手法
-                node_pos[i] += node_disp[i] / np.linalg.norm(node_disp[i]) * tempature
-                node_pos[i] = [min(fullsize, max(0, node_pos[i][w])) for w in range(dim)]
+                #node_pos[i] += node_disp[i] / np.linalg.norm(node_disp[i]) * tempature
+                #node_pos[i] = [min(fullsize, max(0, node_pos[i][w])) for w in range(dim)]
             if count % 2 ==0 and active:
                 memo2[i] = [node_pos[i][w] for w in range(dim)]
 
@@ -243,7 +257,7 @@ def anime_graph():
         if active == False:
             tempature = max(tempature - 0.05, 0)
 
-        # temp = max(temp - 0.1, 0)
+        temp = max(temp - 0.1, 0)
 
         count += 1
 
@@ -277,7 +291,7 @@ def move_rotation(event):
     if dim == 3 and val.get() == 3:
         x = event.x
         y = event.y
-        theta = 2 * (x - x0) * np.pi / fullsize
+        theta = - 2 * (x - x0) * np.pi / fullsize
         phi = 2 * (y - y0) * np.pi / fullsize
         rotation_theta = np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [- np.sin(theta), 0, np.cos(theta)]])
         rotation_phi = np.array([[1, 0, 0], [0, np.cos(phi), - np.sin(phi)], [0, np.sin(phi), np.cos(phi)]])
